416e0ceadacc1a44b046fec5fa961d46189c07a2
Juhi Nayak <juhinayak07@gmail.com>
2020-10-08 15:22:59 -0700

    Bar Chart for Temperature

--- AndroidStudioProjects/PlotGraph/app/src/main/java/com/example/plotgraph/MainActivity.kt
+++ AndroidStudioProjects/PlotGraph/app/src/main/java/com/example/plotgraph/MainActivity.kt
@@ -21,11 +21,54 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        setupBarChartData()
+//        setupBarChartData()
 
-        setupLineChartData()
+//        setupLineChartData()
 
-        getFirestoreTemp()
+//        getFirestoreTemp()
+
+        setupBarChartTemp()
+    }
+
+    private fun setupBarChartData() {
+        // create BarEntry for Bar Group
+
+        val bargroup = ArrayList<BarEntry>()
+
+        bargroup.add(BarEntry(0f, 30f, "0"))
+        bargroup.add(BarEntry(1f, 2f, "1"))
+        bargroup.add(BarEntry(2f, 4f, "2"))
+        bargroup.add(BarEntry(3f, 6f, "3"))
+        bargroup.add(BarEntry(4f, 8f, "4"))
+        bargroup.add(BarEntry(5f, 10f, "5"))
+        bargroup.add(BarEntry(6f, 22f, "6"))
+        bargroup.add(BarEntry(7f, 12.5f, "7"))
+        bargroup.add(BarEntry(8f, 22f, "8"))
+        bargroup.add(BarEntry(9f, 32f, "9"))
+        bargroup.add(BarEntry(10f, 54f, "10"))
+        bargroup.add(BarEntry(11f, 28f, "11"))
+        println(message = "Hello $bargroup")
+
+
+        // creating dataset for Bar Group
+        val barDataSet = BarDataSet(bargroup, "")
+        println(message = "Bar DataSet: $barDataSet")
+
+        barDataSet.color = ContextCompat.getColor(this, R.color.amber)
+
+        val data = BarData(barDataSet)
+        println(message = "Bar Data: $data")
+        barChart.setData(data)
+        barChart.xAxis.position = XAxis.XAxisPosition.BOTTOM
+        barChart.xAxis.labelCount = 11
+        barChart.xAxis.enableGridDashedLine(5f, 5f, 0f)
+        barChart.axisRight.enableGridDashedLine(5f, 5f, 0f)
+        barChart.axisLeft.enableGridDashedLine(5f, 5f, 0f)
+        barChart.description.isEnabled = false
+        barChart.animateY(1000)
+        barChart.legend.isEnabled = false
+        barChart.setPinchZoom(true)
+        barChart.data.setDrawValues(false)
     }
 
     fun getFirestoreTemp(){
@@ -48,23 +91,30 @@ class MainActivity : AppCompatActivity() {
             }
     }
 
-    private fun setupLineChartData() {
+    private fun setupLineChartHumidity() {
         val yVals = ArrayList<Entry>()
-        yVals.add(Entry(0f, 30f, "0"))
-        yVals.add(Entry(1f, 2f, "1"))
-        yVals.add(Entry(2f, 4f, "2"))
-        yVals.add(Entry(3f, 6f, "3"))
-        yVals.add(Entry(4f, 8f, "4"))
-        yVals.add(Entry(5f, 10f, "5"))
-        yVals.add(Entry(6f, 22f, "6"))
-        yVals.add(Entry(7f, 12.5f, "7"))
-        yVals.add(Entry(8f, 22f, "8"))
-        yVals.add(Entry(9f, 32f, "9"))
-        yVals.add(Entry(10f, 54f, "10"))
-        yVals.add(Entry(11f, 28f, "11"))
+
+        var i = 0
+        val db = FirebaseFirestore.getInstance()
+
+        db.collection("cloud-functions-firestore")
+            .get()
+            .addOnSuccessListener { result ->
+                for (document in result) {
+                    val jsonObject = JSONObject(document.data)
+                    val humid = jsonObject.optString("humidity").toFloat()
+                    yVals.add(Entry((i+1).toFloat(),humid))
+                    i++
+                }
+                println(message = "Hello $yVals")
+            }
+            .addOnFailureListener { exception ->
+                Log.w("errordb", "Error getting documents.", exception)
+            }
 
         val set1: LineDataSet
         set1 = LineDataSet(yVals, "DataSet 1")
+        println(message = "Set1: $set1")
 
         // set1.fillAlpha = 110
         // set1.setFillColor(Color.RED);
@@ -82,7 +132,9 @@ class MainActivity : AppCompatActivity() {
 
         val dataSets = ArrayList<ILineDataSet>()
         dataSets.add(set1)
+        println(message = "dataset: $dataSets")
         val data = LineData(dataSets)
+        println(message = "LineData $data")
 
         // set data
         lineChart.setData(data)
@@ -93,44 +145,51 @@ class MainActivity : AppCompatActivity() {
         lineChart.axisRight.enableGridDashedLine(5f, 5f, 0f)
         lineChart.axisLeft.enableGridDashedLine(5f, 5f, 0f)
         //lineChart.setDrawGridBackground()
-        lineChart.xAxis.labelCount = 11
+        lineChart.xAxis.labelCount = 20
         lineChart.xAxis.position = XAxis.XAxisPosition.BOTTOM
     }
 
-    private fun setupBarChartData() {
+    private fun setupBarChartTemp() {
         // create BarEntry for Bar Group
 
+        val db = FirebaseFirestore.getInstance()
         val bargroup = ArrayList<BarEntry>()
+        var i = -1
 
-        bargroup.add(BarEntry(0f, 30f, "0"))
-        bargroup.add(BarEntry(1f, 2f, "1"))
-        bargroup.add(BarEntry(2f, 4f, "2"))
-        bargroup.add(BarEntry(3f, 6f, "3"))
-        bargroup.add(BarEntry(4f, 8f, "4"))
-        bargroup.add(BarEntry(5f, 10f, "5"))
-        bargroup.add(BarEntry(6f, 22f, "6"))
-        bargroup.add(BarEntry(7f, 12.5f, "7"))
-        bargroup.add(BarEntry(8f, 22f, "8"))
-        bargroup.add(BarEntry(9f, 32f, "9"))
-        bargroup.add(BarEntry(10f, 54f, "10"))
-        bargroup.add(BarEntry(11f, 28f, "11"))
+        db.collection("cloud-functions-firestore")
+            .get()
+            .addOnSuccessListener { result ->
+                for (document in result) {
+                    val jsonObject = JSONObject(document.data)
+                    val temp = jsonObject.optString("temperature").toFloat()
+                    bargroup.add(BarEntry((i+1).toFloat(),temp))
+                    i++
+                }
+                println(message = "Hello $bargroup")
 
-        // creating dataset for Bar Group
-        val barDataSet = BarDataSet(bargroup, "")
+                // creating dataset for Bar Group
+                val barDataSet = BarDataSet(bargroup, "Bar DataSet")
+                println(message = "Bar DataSet: $barDataSet")
 
-        barDataSet.color = ContextCompat.getColor(this, R.color.amber)
+                barDataSet.color = ContextCompat.getColor(this, R.color.amber)
 
-        val data = BarData(barDataSet)
-        barChart.setData(data)
-        barChart.xAxis.position = XAxis.XAxisPosition.BOTTOM
-        barChart.xAxis.labelCount = 11
-        barChart.xAxis.enableGridDashedLine(5f, 5f, 0f)
-        barChart.axisRight.enableGridDashedLine(5f, 5f, 0f)
-        barChart.axisLeft.enableGridDashedLine(5f, 5f, 0f)
-        barChart.description.isEnabled = false
-        barChart.animateY(1000)
-        barChart.legend.isEnabled = false
-        barChart.setPinchZoom(true)
-        barChart.data.setDrawValues(false)
+                val data = BarData(barDataSet)
+                println(message = "Bar Data: $data")
+                barChart.setData(data)
+                barChart.xAxis.position = XAxis.XAxisPosition.BOTTOM
+                barChart.xAxis.labelCount = i
+                barChart.xAxis.enableGridDashedLine(5f, 5f, 0f)
+                barChart.axisRight.enableGridDashedLine(5f, 5f, 0f)
+                barChart.axisLeft.enableGridDashedLine(5f, 5f, 0f)
+                barChart.description.isEnabled = false
+                barChart.animateY(1000)
+                barChart.legend.isEnabled = false
+                barChart.setPinchZoom(true)
+                barChart.data.setDrawValues(false)
+            }
+            .addOnFailureListener { exception ->
+                Log.w("errordb", "Error getting documents.", exception)
+            }
+
     }
 }
